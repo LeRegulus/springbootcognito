fac5c08c09470cd20bfe6fdb6cebdcd49d970f43
Alassane Seck <alassane.seck@m2t.biz>
2024-04-08 11:14:27 +0000

    Initioalisation

---
+++ src/main/java/com/regulus/sprinbootcognito/SprinbootcognitoApplication.java
@@ -0,0 +1,13 @@
+package com.regulus.sprinbootcognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootcognitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbootcognitoApplication.class, args);
+    }
+
+}
